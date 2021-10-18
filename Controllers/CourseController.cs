27cdf1a502d5f1efba7f9130b8b5fee9409a076e
Tushar <86506163+Tks008@users.noreply.github.com>
2021-10-18 07:00:49 +0530

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using sqlwebapp.Models;
+using sqlwebapp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlwebapp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _CourseService;
+        private readonly IConfiguration _Configuration;
+        public CourseController(CourseService courseService, IConfiguration configuration)
+        {
+            _CourseService = courseService;
+            _Configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            var data = _CourseService.GetCourses(_Configuration.GetConnectionString("SQLconnection"));
+            return View(data);
+        }
+    }
+}
---
+++ Service/CourseService.cs
@@ -0,0 +1,47 @@
+﻿using sqlwebapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlwebapp.Service
+{
+    public class CourseService
+    {
+        public List<Course> GetCourses(string conn)
+        {
+
+            List<Course> course = new List<Course>();
+            //{
+            //    new Course() { ID = 100, Name = "AZ-900", Rating = 4 },
+            //    new Course() { ID = 101, Name = "AZ-204", Rating = 7 },
+            //    new Course() { ID = 101, Name = conn, Rating = 7 }
+            //};
+            string _statement = "SELECT ID,Name,rating from Course";
+            SqlConnection _connection = new SqlConnection(conn);
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        ID = _reader.GetInt32(0),
+                        Name = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    course.Add(_course);
+                }
+            }
+            _connection.Close();
+
+            return course;
+        }
+    }
+}
